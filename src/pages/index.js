62e64e815c9f59900abf48f35d16d58bf7396b09
murokaco <murokaco@gmail.com>
2019-01-11 01:32:51 +0900

    eslintの言うことを聞いた

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Link, graphql } from 'gatsby'
 import Layout from '../components/Layout'
+import { graphql, Link } from 'gatsby'
 
 export default class IndexPage extends React.Component {
   render() {
