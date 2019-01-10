c1e45b68b5daaa56ed1d86b5d20d61bb64e85268
murokaco <murokaco@gmail.com>
2019-01-11 03:14:08 +0900

    OGP変更

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
-    description: 'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+    title: 'かこログ',
+    description: '日々勉強していることを過去ログとして残していきます。',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
