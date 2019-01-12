b6ad01931823dda4b452508aac88fa6814ee545d
murokaco <murokaco@gmail.com>
2019-01-13 03:28:47 +0900

    footerを追加

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = class extends React.Component {
+  render () {
+    return (
+      <footer className="l-footer">
+        <p>©<a href="https://twitter.com/murokaco" className="l-footer__link">murokaco</a></p>
+      </footer>
+    )
+  }
+
+}
+
+export default Footer
