053bd2063724693b0334f6d1fa630d8cd8ef8623
murokaco <murokaco@gmail.com>
2019-01-15 23:09:24 +0900

    classじゃなくて関数で定義する

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,9 @@
 import React from 'react'
 
-const Footer = class extends React.Component {
-  render () {
-    return (
-      <footer className="l-footer">
-        <p>©<a href="https://twitter.com/murokaco" className="l-footer__link">murokaco</a></p>
-      </footer>
-    )
-  }
-
-}
+const Footer = () => (
+  <footer className="l-footer">
+    <p>©<a href="https://twitter.com/murokaco" className="l-footer__link">murokaco</a></p>
+  </footer>
+)
 
 export default Footer
